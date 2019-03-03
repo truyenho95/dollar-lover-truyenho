0caef7311521090d73b95a2dec0e9c69d349cdce
truyenho95 <truyenho.webdev@gmail.com>
2019-03-03 10:32:56 +0700

    Add GetMapping("/") in WalletControler return view index

--- src/main/java/com/truyenho/dollarlover/controller/WalletController.java
+++ src/main/java/com/truyenho/dollarlover/controller/WalletController.java
@@ -1,4 +1,15 @@
 package com.truyenho.dollarlover.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class WalletController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("/index");
+    }
+
 }
