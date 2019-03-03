345397168ce4e7b19d44109877c5e25f76165a21
truyenho95 <truyenho.webdev@gmail.com>
2019-03-03 10:25:25 +0700

    first commit

---
+++ src/main/java/com/truyenho/dollarlover/DollarLoverApplication.java
@@ -0,0 +1,13 @@
+package com.truyenho.dollarlover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarLoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DollarLoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truyenho/dollarlover/DollarLoverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.truyenho.dollarlover;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DollarLoverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
