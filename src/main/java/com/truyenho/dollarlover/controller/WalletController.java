b87478e9fc7ac4e47f504b5fb9f2c850077b761e
truyenho95 <truyenho.webdev@gmail.com>
2019-03-03 22:46:08 +0700

    Add annotation @CrossOrigin

--- src/main/java/com/truyenho/dollarlover/controller/WalletController.java
+++ src/main/java/com/truyenho/dollarlover/controller/WalletController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WalletController {
     @Autowired
     private WalletService walletService;
--- src/main/java/com/truyenho/dollarlover/repository/WalletRepository.java
+++ src/main/java/com/truyenho/dollarlover/repository/WalletRepository.java
@@ -3,7 +3,9 @@ package com.truyenho.dollarlover.repository;
 import com.truyenho.dollarlover.model.Wallet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository("WalletRepository")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public interface WalletRepository extends CrudRepository<Wallet, Long> {
 }
